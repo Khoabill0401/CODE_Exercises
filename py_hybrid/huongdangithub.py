e85d2b7bc8cb91eae5b2624766db0276c492fb37
Khoabill0401 <khoabill0401@gmail.com>
2021-10-24 23:18:44 +0700

    sixth commit

--- py_hybrid/huongdangithub.py
+++ py_hybrid/huongdangithub.py
@@ -4,9 +4,8 @@ To push
 2. git commit -m "text here"
 3. git push
 
-To pull - là ở 1 máy khác m vừa thay đổi code, và đã được push lên
-            m không muốn cứ clone về mãi, thì ở cái thư mục clone về cũ
-            chỉ cần gõ git pull là nó update code mới nhất cho m
+To pull - nếu vừa thay đổi code, và đã được push lên thì ở thư mục clone về cũ
+          chỉ cần gõ git pull là update code mới nhất
 1. git pull
 2. code st
 3. git add .
